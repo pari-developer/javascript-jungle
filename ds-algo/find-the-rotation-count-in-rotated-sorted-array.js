85df69e36195316df234f2b54620cf5ba213f940
Pranita Panigrahi <pranitapanigrahi50@gmail.com>
2022-02-23 14:09:10 +0530

    fixed naming convention

--- ds-algo/find-the-rotation-count-in-rotated-sorted-array.js
+++ ds-algo/find-the-rotation-count-in-rotated-sorted-array.js
@@ -1,31 +1,34 @@
-//Consider an array of distinct numbers sorted in increasing order. 
-//The array has been rotated (clockwise) k number of times. Given such an array, find the value of k.
+//Consider an arr of distinct numbers sorted in increasing order. 
+//The arr has been rotated (clockwise) k number of times. Given such an arr, find the value of k.
 //Input : arr= [15, 18, 2, 3, 6, 12]
 //Output: 2
 
-function countRotations(arr){
-   let Pivot = findPivot(arr);
-   if(Pivot === -1){
-       return 0;
-   }
-    return Pivot+1
-   
+function countRotations(arr) {
+    let pivot = findPivot(arr);
+    if (pivot === -1) {
+        return 0;
+    }
+    return pivot + 1
+
 }
 
-function findPivot(array) {
- let start = 0;
- let end = array.length - 1;
- while(start < end){
-     let mid = Math.floor((start + end)/2);
-     if(mid < end && array[mid] > array[mid+1]){
-         return mid ; 
-     }else if( mid > start && array[mid]<array[mid-1]){
-         return mid-1;
-     }else if(arr[start] > arr[mid]){
-         end = mid - 1
-     }else {
-         start + 1;
-     }
- }
- return -1;
+function findPivot(arr) {
+    let start = 0;
+    let end = arr.length - 1;
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2);
+        if (mid < end && arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+        if (mid > start && arr[mid] < arr[mid - 1]) {
+            return mid - 1;
+        }
+
+        if (arr[start] > arr[mid]) {
+            end = mid - 1
+        } else {
+            start + 1;
+        }
+    }
+    return -1;
 }
